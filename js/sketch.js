80f77f0f55efa7294367522c1791c8c4b69bab43
Indy <116882977+indyyd@users.noreply.github.com>
2022-12-02 11:41:37 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -7,7 +7,8 @@ function setup() {
 
 
     for(var i = 0; i < 100; i++) {
-        circles();
+        circles1();
+        circles2();
     }
      //'calling' the function into setup
 }
@@ -16,11 +17,22 @@ function draw() {
 
 }
 
-function circles() {
+function circles1() {
     var x = random(width);
     var y = random(height);
     var d = random(5, 150);
     var hue = random(120, 255);
+
+    noStroke();
+    fill(hue, 255, 255, 0.1);
+    circle(x, y, d);
+}
+
+function circles2() {
+    var x = random(width);
+    var y = random(height);
+    var d = random(5, 150);
+    var hue = random(20, 120);
 
     noStroke();
     fill(hue, 255, 255, 0.1);
